39abc15a354d1a52e96bb25672397782e89eaabc
Kimd0ng <kdongh0406@gmail.com>
2026-07-22 15:42:48 +0900

    feat: TanStack Vue Query 배선

--- src/app/main.js
+++ src/app/main.js
@@ -2,13 +2,16 @@ import '../style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 import App from './App.vue'
 import router from './router'
+import { queryClient } from './queryClient'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin, { queryClient })
 
 app.mount('#app')
