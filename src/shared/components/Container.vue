22d663f5f3e2e644bbb53480078fe270fa4df01b
Kimd0ng <kdongh0406@gmail.com>
2026-07-28 12:05:30 +0900

    feat: 공용 Container 리스트 셀 컴포넌트 추가

---
+++ src/shared/components/Container.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineOptions({ name: 'AppContainer' })
+
+defineProps({
+  clickable: { type: Boolean, default: false },
+})
+
+defineEmits(['click'])
+</script>
+
+<template>
+  <component
+    :is="clickable ? 'button' : 'div'"
+    :type="clickable ? 'button' : undefined"
+    class="flex w-full items-center gap-3 border-b border-[#111110]/5 py-3 text-left last:border-b-0"
+    @click="clickable ? $emit('click', $event) : undefined"
+  >
+    <div v-if="$slots.leading" class="shrink-0">
+      <slot name="leading" />
+    </div>
+    <div class="min-w-0 flex-1">
+      <slot />
+    </div>
+    <div v-if="$slots.trailing" class="shrink-0">
+      <slot name="trailing" />
+    </div>
+  </component>
+</template>
