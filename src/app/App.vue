2eef42a664b6c9e48c1fd2b8c03b765fb4c72a8d
Kimd0ng <kdongh0406@gmail.com>
2026-07-28 12:58:58 +0900

    feat: 닫기 아이콘 및 모달 렌더링 루트 추가

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,6 +1,8 @@
 <script setup>
+import ModalRoot from '@/shared/components/ModalRoot.vue'
 </script>
 
 <template>
   <router-view />
+  <ModalRoot />
 </template>
---
+++ src/shared/components/ModalRoot.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useModalStore } from '@/shared/store/modal'
+import BaseModal from './BaseModal.vue'
+
+const modalStore = useModalStore()
+</script>
+
+<template>
+  <BaseModal>
+    <component :is="modalStore.component" v-bind="modalStore.props" />
+  </BaseModal>
+</template>
