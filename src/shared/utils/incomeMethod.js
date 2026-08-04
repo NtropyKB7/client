91184cf4e7dba557f123cb78490cb90868e8a1c6
Kimd0ng <kdongh0406@gmail.com>
2026-08-04 13:40:42 +0900

    feat: 잡 추가/수정 폼에 소득방식 인라인 드롭다운 추가

--- src/shared/utils/incomeMethod.js
+++ src/shared/utils/incomeMethod.js
@@ -1,5 +1,5 @@
 /**
- * JobCard 편집 폼의 소득방식 피커에서 사용.
+ * JobFormModal의 소득방식 인라인 드롭다운에서 사용.
  * '건당 정산' 문자열은 calendar/utils.js의 computeEntryIncome이 직접 비교하므로 값을 바꾸지 말 것.
  */
 export const INCOME_METHOD_OPTIONS = ['건당 정산', '시급', '월 정산']
